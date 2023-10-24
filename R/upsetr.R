49234290aac50271ae207bc11ed6d9cb5e39613a
dereeper <alexis.dereeper@ird.fr>
2023-10-24 16:55:13 +0200

    update upsetr.R

--- R/upsetr.R
+++ R/upsetr.R
@@ -1,7 +1,7 @@
 #!/usr/bin/R
 
 library(UpSetR)
-library(svglite)
+#library(svglite)
 args = commandArgs(trailingOnly=TRUE)
 
 
@@ -15,8 +15,8 @@ if (as.integer(nb)>60){
 }
 
 sets <- names(movies[2:nb])
-svglite(args[2], width = 4, height = 4)
-#pdf(args[2])
-upset(movies,nintersects = 20,number.angles = 0, mb.ratio = c(0.35, 0.65),order.by = "freq", sets = sets, point.size = 0.5, line.size = 0.3, mainbar.y.label = "Intersections", sets.x.label = "Nb genes", text.scale = c(0.8, 0.8, 0.8, 0.8, size, 0.5))
+#svglite(args[2], width = 4, height = 4)
+pdf(args[2])
+#upset(movies,nintersects = 20,number.angles = 0, mb.ratio = c(0.35, 0.65),order.by = "freq", sets = sets, point.size = 0.5, line.size = 0.3, mainbar.y.label = "Intersections", sets.x.label = "Nb genes", text.scale = c(0.8, 0.8, 0.8, 0.8, size, 0.5))
+upset(movies,nintersects = 20,number.angles = 0, mb.ratio = c(0.35, 0.65), order.by = "freq",keep.order = T, sets = sets, point.size = 0.5, line.size = 0.3, mainbar.y.label = "Intersections", sets.x.label = "Nb genes", text.scale = c(0.8, 0.8, 0.8, 0.8, size, 0.5))
 dev.off()
-
