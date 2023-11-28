ffa2c4d43b83f8ec847260c3be95ceec3adbd96a
dereeper <alexis.dereeper@ird.fr>
2023-11-28 16:20:20 +0100

    update script for managing yml config file

--- Perl/generateConfig.pl
+++ Perl/generateConfig.pl
@@ -1,30 +1,47 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
+use YAML qw(DumpFile);
+
+use Cwd;
+my $dir = getcwd;
+
 
 my $zip = $ARGV[0];
-my $out = $ARGV[1];
+my $list = $ARGV[1];
+my $out = $ARGV[2];
 
-open(O,">$out");
-if (-e $zip){
+$list =~s/NZ_//g;
+my @list_ids = split(/,/,$list);
+
+my %data = ();
+foreach my $id(@list_ids){
+	push @{$data{"ids"}}, "$id";
+}
+if ($zip ne "None"){
 	system("rm -rf $zip.genomeszip");
 	mkdir("$zip.genomeszip");
 	chdir("$zip.genomeszip");
 	my $head = `head -1 $zip`;
 	if ($head =~/LOCUS/){
-		print "Dans ce cas\n";
-		print O "$zip\n";
+		push @{$data{"input_genbanks"}}, "$zip";
 		exit;
 	}
 	system("cp -rf $zip ./genomes.zip");
 	system("unzip genomes.zip");
 	unlink("genomes.zip");
 	open(LS,"ls |");
-	while(<LS>){
-		print O "$zip.genomeszip/".$_;
+	while(my $line = <LS>){
+		chomp($line);
+		my $concat = "$zip.genomeszip/".$line;
+		push @{$data{"input_genbanks"}}, "$concat";
 	}
 	close(LS);
-	
 }
-close(O);
+#else{
+#	push @{$data{"input_genbanks"}}, "";
+#}
+chdir($dir);
 
+DumpFile($out, \%data);
