261857d95b07a65f8212279a97341c5d09786c94
dereeper <alexis.dereeper@ird.fr>
2025-02-25 20:58:56 +0100

    Update get_data.pl with correct parameters of datasets

--- Perl/get_data.pl
+++ Perl/get_data.pl
@@ -63,7 +63,7 @@ while(my $line =<F>){
 	if (!-e "$genbank"){
 
 		my $assembly_accession = $genbank;
-		system("datasets download genome accession --include gbff --filename $outdir/$assembly_accession.zip $assembly_accession");
+		system("datasets download genome accession --include genome,gbff --filename $outdir/$assembly_accession.zip $assembly_accession");
 		system("unzip -o $outdir/$assembly_accession.zip");
 		system("cp -rf ncbi_dataset/data/$assembly_accession/$assembly_accession*genomic.fna $outdir/$genbank.fasta");
 		system("cp -rf ncbi_dataset/data/$assembly_accession/genomic.gbff $outdir/$genbank.gb");
