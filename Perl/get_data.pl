22ab06eda607b7f02cfb85d5af416898da56952a
dereeper <alexis.dereeper@ird.fr>
2026-02-16 05:40:07 +0100

    Update file copy commands for genomic data

--- Perl/get_data.pl
+++ Perl/get_data.pl
@@ -65,8 +65,8 @@ while(my $line =<F>){
 		my $assembly_accession = $genbank;
 		system("datasets download genome accession --api-key 9dd2835d3f49969cf0182b2a9bd8b1256e08 --include genome,gbff --filename $outdir/$assembly_accession.zip $assembly_accession");
 		system("unzip -o $outdir/$assembly_accession.zip");
-		system("cp -rf ncbi_dataset/data/$assembly_accession/*genomic.fna $outdir/$genbank.fasta");
-		system("cp -rf ncbi_dataset/data/$assembly_accession/genomic.gbff $outdir/$genbank.gb");
+		system("cp -rf ncbi_dataset/data/$assembly_accession*/*genomic.fna $outdir/$genbank.fasta");
+		system("cp -rf ncbi_dataset/data/$assembly_accession*/genomic.gbff $outdir/$genbank.gb");
                 sleep(5); 
 		
 	}
